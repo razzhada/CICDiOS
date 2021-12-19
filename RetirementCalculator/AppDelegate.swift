32565c055e78d60b676f6ee0460e199d55aec725
Hadi Razzak <hadi_razzak@huntsman.com>
2021-12-19 16:51:12 -0600

    Dummy  change

--- RetirementCalculator/AppDelegate.swift
+++ RetirementCalculator/AppDelegate.swift
@@ -22,7 +22,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+// Dummy change
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
